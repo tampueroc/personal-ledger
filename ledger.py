0e66913a77b5303c97120e59a1e87b695822407f
Tom Ampuero <tomas.ampuero.c@ug.uchile.cl>
2022-06-16 20:52:30 -0400

    feat: config validation

--- ledger.py
+++ ledger.py
@@ -1,4 +1,5 @@
-from ledger_models import Transaction
+import re
+from ledger_models import Account, Transaction
 
 
 class Ledger:
@@ -6,9 +7,27 @@ class Ledger:
         """
         Initialize ledger file record
         """
-        self._ledger = open("ledger_book.ledger", "a")
+        self._ledger_file = ".ledger"
+        try:
+            self.valid_account_config()
+        except IOError:
+            self._ledger = open(self._ledger_file, "w")
+            init_config = ";Config\n" + ";;Accounts\n" + "\tExpenses\n" + "\tAssets\n" + "\tIncome\n" + "\tLiabilities\n" + "\tEquity\n" 
+            self._ledger.write(init_config)
 
+    def valid_account_config(self):
+        with open(self._ledger_file) as ledger_file:
+            for line in ledger_file:
+                line = re.search(";;Accounts", line)
+                if line:
+                    return True
+            raise IOError("No Account config found")
+                
     def add_transaction(self, transaction_data: Transaction):
+        self._ledger = open(self._ledger_file, "a")
         # Write into .ledger
         self._ledger.write(transaction_data.__str__())
+    
+
+        
         
